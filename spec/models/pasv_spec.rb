afacc6ff60eafe6f7cdb8046b14738a6a27bfc99
Ryan Moore <moorer@udel.edu>
2019-02-13 20:36:46 -0500

    Use shoulda

--- spec/models/pasv_spec.rb
+++ spec/models/pasv_spec.rb
@@ -1,46 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Pasv, type: :model do
-  subject do
-    described_class.new queries_fname: "queries.fa",
-                        refs_fname: "refs.fa",
-                        aligner: "clustalo",
-                        roi_start: -1,
-                        roi_end: -1
-  end
-
-
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
-  end
-
-  it "is not valid without a queries_fname" do
-    subject.queries_fname = nil
-
-    expect(subject).not_to be_valid
-  end
-
-  it "is not valid without a refs_fname" do
-    subject.refs_fname = nil
-
-    expect(subject).not_to be_valid
-  end
-
-  it "is not valid without an aligner" do
-    subject.aligner = nil
-
-    expect(subject).not_to be_valid
-  end
-
-  it "is not valid without an roi_start" do
-    subject.roi_start = nil
-
-    expect(subject).not_to be_valid
-  end
-
-  it "is not valid without an roi_end" do
-    subject.roi_end = nil
-
-    expect(subject).not_to be_valid
-  end
+  it { is_expected.to validate_presence_of :queries_fname }
+  it { is_expected.to validate_presence_of :refs_fname }
+  it { is_expected.to validate_presence_of :aligner }
+  it { is_expected.to validate_presence_of :roi_start }
+  it { is_expected.to validate_presence_of :roi_end }
 end
